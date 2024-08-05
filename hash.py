6bb20c13defbaa12808a7000b0fc5ea93e00cd37
Thành qwerty <dtthanh2909@gmail.com>
2024-08-05 16:09:44 +0700

    delete hash

--- hash.py
+++
@@ -1,8 +0,0 @@
-from passlib.context import CryptContext
-pwd_context = (CryptContext(schemes=["bcrypt"], deprecated="auto"))
-class hash():
-    def bcrypt(password: str):
-        return pwd_context.hash(password)
-
-    def verify(plain_password: str,hashed_password: str):
-        return pwd_context.verify(plain_password, hashed_password)
